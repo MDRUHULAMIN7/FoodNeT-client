83bf0ff9227acc7e74bac6a60adb60affdc67e9e
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-05-12 01:19:18 +0600

    mamba ui

--- src/Components/Foodcard.jsx
+++ src/Components/Foodcard.jsx
@@ -1,44 +1,56 @@
 import { Link } from "react-router-dom";
 
+const Foodcard = ({ food }) => {
+  const {
+    donatorname,
+    _id,
+    donatorphoto,
+    name,
+    image,
+    quantity,
+    location,
+    date,
+    foodstatus,
+    additonalnotes,
+  } = food;
+  return (
+    <div className="rounded-md shadow-md  dark:bg-gray-50 dark:text-gray-800 ">
+	<div className="h-1/2">
+    <div className="flex items-center justify-between p-3">
+		<div className="flex items-center space-x-2">
+			<img src={donatorphoto} alt="" className="object-cover object-center w-10 h-10 rounded-full shadow-sm dark:bg-gray-500 dark:border-gray-300" />
+			<div className="-space-y-1">
+				<h2 className="text-sm font-semibold leading-none">{donatorname}</h2>
+				
+			</div>
+		</div>
+		<h1 className="px-1 py-1 bg-green-500 rounded-lg">{foodstatus}</h1>
+	</div>
+	<img src={image} alt="" className="object-cover object-center w-full h-60 dark:bg-gray-500" />
+    </div>
+	<div className="p-3 h-1/2">
+		<div className="flex items-center justify-between">
+			<div className="flex items-center space-x-3">
+				
+			</div>
+			
+		</div>
+		<div className=" pt-3 pb-1">
+			<div className="flex items-center gap-5">
+			
+			<h1 className="text-2xl">{name}</h1> <h1 className="text-xl">{location}</h1>
+			</div>
+		</div>
+		<div className="flex space-x-2 ">
+			<h1>ExpireDate:{date}</h1> , <h1>Quantity:{quantity}</h1>
+		
+		</div>
+        <p>{additonalnotes.slice(0,30)}</p>
 
-const Foodcard = ({food}) => {
-    const {donatorname,_id,donatorphoto,name,image,quantity,location,date,foodstatus,additonalnotes}=food
-    return (
-        <div className="h-96  hover:shadow-2xl hover:shadow-rose-500">
-            <div className="h-2/5 w-full">
-            <div      className=" mb-2 bg-cover hero   h-full w-full"
-        style={{
-          backgroundImage: `url(${image})`,
-        }}>
-        
-            <div className="flex mt-[108px] w-full justify-between px-1 "><h1 className="bg-green-500 text-xl text-center py-2 px-2 ">{foodstatus}</h1> <h1 className="bg-rose-600 text-xl  px-2 py-2 ">Quantity : {quantity}</h1></div>
-
-            </div>
-            </div>
-       
-            <hr />
-            <div className="h-3/5 mt-2  ">
-               <div className="px-2">
-               <div className="flex my-2 justify-between items-center text-2xl ">
-                    <img className="h-14 border-2 border-rose-600 rounded-full" src={donatorphoto} alt="" /> <h1>Donateby:{donatorname}</h1>
-                </div>
-                <hr />
-                <div>
-<p className="flex justify-between my-2"><h1 className="text-2xl font-semibold">{name}</h1>
-<h1 className="text-xl">{location}</h1></p>
-<h1 className="font-semibold ">ExpireDate:{date}</h1>
-<p>{additonalnotes.slice(0,40)}</p>
-               </div>
-
-<Link className="" to={`/fooddetail/${_id}`}>
-<button className="w-full bg-rose-600 hover:bg-black  py-2  mt-1 text-white font-semibold" title="Click to Show Details">View Details</button>
-</Link>
-                </div>
-
-            </div>
-       
-        </div>
-    );
+        <Link to={`/fooddetail/${_id}`} className="w-full bg-rose-600 py-2 mt-2 text-right"><button>View Details</button></Link>
+	</div>
+</div>
+  );
 };
 
 export default Foodcard;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,14 @@
 
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const Root = () => {
     return (
         <div className="md:mx-10 mx-3">
            <Navbar></Navbar>
          <Outlet></Outlet> 
-       
+       <Footer></Footer>
         </div>
     );
 };
